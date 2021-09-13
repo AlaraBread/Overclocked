2fc7af1737e29031790d5eaceefdb1970cfffd10
Julian <jul204040204625@gmail.com>
2021-09-13 08:56:02 -0400

    slightly adjust moving platforms to handle negative offsets more gracefully

--- game/objects/MovingPlatform.gd
+++ game/objects/MovingPlatform.gd
@@ -7,23 +7,17 @@ var initial_pos:Vector2
 var end_pos:Vector2
 var dir:Vector2
 func _ready():
-	if(offset.x < 0):
-		position.x += offset.x
-		offset.x *= -1
-	if(offset.y < 0):
-		position.y += offset.y
-		offset.y *= -1
 	initial_pos = position
 	end_pos = position+offset
 	dir = offset.normalized()
 
 func _physics_process(delta:float):
 	position += dir*(speed*delta*time_scale)
-	if((position-end_pos).length_squared() < 10):
-		dir = -Vector2(abs(dir.x), abs(dir.y))
+	if(speed > 0 and (position-end_pos).length_squared() < 10):
+		speed *= -1
 		constant_linear_velocity = dir*(speed*time_scale)
-	elif((position-initial_pos).length_squared() < 10):
-		dir = Vector2(abs(dir.x), abs(dir.y))
+	elif(speed < 0 and (position-initial_pos).length_squared() < 10):
+		speed *= -1
 		constant_linear_velocity = dir*(speed*time_scale)
 
 var time_scale:float = 1
