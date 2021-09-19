1667180b6538a873045231cb5ea9c40696e606eb
Julian <jul204040204625@gmail.com>
2021-09-19 00:08:36 -0400

    stuff

---
+++ game/PauseMenu.gd
@@ -0,0 +1,28 @@
+extends Control
+
+func _on_PauseButton_pressed():
+	set_paused(true)
+
+func _input(event):
+	if(Input.is_action_just_pressed("pause")):
+		set_paused(not get_tree().paused)
+
+func set_paused(p:bool):
+	if(get_node("../End").visible):
+		p = false
+	get_tree().paused = p
+	get_node("../PauseButton").visible = not p
+	visible = p
+	if(p):
+		$Unpause.grab_focus()
+
+func _on_Unpause_pressed():
+	set_paused(false)
+
+func _on_Reset_pressed():
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+
+func _on_MainMenu_pressed():
+	get_tree().paused = false
+	get_tree().change_scene("res://menus/MainMenu.tscn")
